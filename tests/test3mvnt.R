838fd8f9d64a89da2d8e72503e0d8b20f01286c0
MA Jian <majian03@gmail.com>
2024-06-07 23:40:02 +0000

    version 0.5

--- tests/test3mvnt.R
+++
@@ -1,7 +0,0 @@
-library(copent)
-library(mnormt)
-rho <- 0.5
-sigma <- matrix(c(1,rho,rho,1),2,2)
-x <- rmnorm(1000,c(0,0),sigma)
-mvnt(x)
-
--- tests/test4tst.R
+++
@@ -1,8 +0,0 @@
-library(copent)
-library(mnormt)
-rho <- 0.5
-sigma <- matrix(c(1,rho,rho,1),2,2)
-s0 <- rmnorm(400,c(0,0),sigma)
-s1 <- rmnorm(500,c(5,5),sigma)
-tst(s0,s1)
-
